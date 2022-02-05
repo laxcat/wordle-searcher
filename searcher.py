bc17e97ed01b1e31c53b0b0e094dd1efbad0f8eb
Tyler Martin <tyler@tylermartin.net>
2022-02-05 13:09:04 -0700

    add more complex search patterns

--- searcher.py
+++ searcher.py
@@ -8,7 +8,11 @@ try:
     while(True):
         user_input = input('Input search pattern. Lowercase for yellows, uppercase for greens, dashes for greys.\n')
 
-        if '|' not in user_input and len(user_input) != 5:
+        is_complex = bool('|' in user_input)
+        has_gray = bool(',' in user_input)
+
+        # reject user input if not 5 letters and not a complex pattern
+        if not is_complex and not has_gray and len(user_input) != 5:
             continue
 
         # will form pattern that expects a green in the proper slot, 
@@ -16,26 +20,36 @@ try:
         pattern = r''
         # we'll capture yellows and just make sure the word contains them.
         yellows = []
+        grays = [] 
 
+        # comma at end of pattern indicates list of grays, to ensure exclude words that have them
+        if has_gray:
+            temp = user_input.split(',')
+            user_input = temp[0]
+            grays = list(temp[1])
 
-        if '|' in user_input:
+        # if complex parsing detected, "letters" could be groups of letters 
+        # (but are treated the same way when making the pattern)
+        if is_complex:
             user_input = user_input.split('|')
 
-        # each letter of input
-        for letter in user_input:
-            if letter.isupper():
-                pattern += letter.lower()
-            elif letter.islower():
-                pattern += f'[^{letter}]'
-                for letter_part in letter:
-                    if letter_part not in yellows:
-                        yellows.append(letter_part)
+        # each part (letter or group letters) of input
+        for part in user_input:
+            if part.isupper():
+                pattern += part.lower()
+            elif part.islower():
+                pattern += f'[^{part}]'
+                # might be group of letters or single part
+                for letter in part:
+                    if letter not in yellows:
+                        yellows.append(letter)
             # expecting dash but could be anything that doesn't pass isupper && islower
             else:
                 pattern += '[a-z]'
 
         y_str = ','.join(yellows)
-        print(f'pattern = {pattern}, contains = {y_str}')
+        g_str = ','.join(grays)
+        print(f'pattern = {pattern}  contains = {y_str}  excludes = {g_str}')
         found = []
 
         # each word in big list
@@ -46,6 +60,10 @@ try:
                 passed = True
                 for y in yellows:
                     if y not in word:
+                        passed = False
+                        break
+                for g in grays:
+                    if g in word:
                         passed = False
                         break
                 if passed:
